45225dc968f74708b891a2017502d13a5061f407
gurunayak7 <gurunayak2301@gmail.com>
2025-02-16 11:34:23 +0530

    SUBSCRIPTION_mERGE

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,47 +1,21 @@
-<<<<<<< HEAD
-    import React, { useState } from "react";
-    import TopBar from "../component/TopBar";
-    import Sidebar from "../component/Sibebar";
-    import Dashboard from "../component/Dashboard";
-    import ManageExam from "../component/ManageExam";
-    import ManageResult from "../component/ManageResult";
-    import ManageStudents from "../component/ManageStudents";
-=======
 import React, { useState } from "react";
 import TopBar from "../component/TopBar";
 import Sidebar from "../component/Sibebar";
 import Dashboard from "../component/Dashboard";
 import ManageExam from "../component/ManageExam";
 import NewExam from "../component/NewExam";
+import ManageResult from "../component/ManageResult";
+import ManageStudents from "../component/ManageStudents";
 import AddQuestion from "../component/AddQuestion";
 import AddStudents from "../component/AddStudents";
 import Swal from "sweetalert2";
 import Subcription from "../component/Subcription";
 import Settings from "../component/Settings";
->>>>>>> 95f3d444caf171b58ad22eb31db068b9133a1b5a
 
-    const Home = () => {
-        const [activeComponent, setActiveComponent] = useState("dashboard");
+const Home = () => {
+    const [activeComponent, setActiveComponent] = useState("dashboard");
 
-<<<<<<< HEAD
-        return (
-            <div>
-                <TopBar />
-                <div className="flex">
-                    {/* Sidebar */}
-                    <div className="xl:w-2/10 lg:w-2/10 md:w-0/10 sm:w-0">
-                        <Sidebar setActiveComponent={setActiveComponent} />
-                    </div>
-
-                    {/* Main Content Area */}
-                    <div className="xl:w-8/10 lg:w-8/10 md:w-10/10 sm:w-full">
-                        {activeComponent === "dashboard" && <Dashboard />}
-                        {activeComponent === "manageExam" && <ManageExam />}
-                        {activeComponent === "result" && <ManageResult />}
-                        {activeComponent === "student" && <ManageStudents />}
-                    </div>
-                </div>  
-=======
+    // Function to handle exam creation alert
     const handleCreateExam = () => {
         Swal.fire({
             title: "Saved!",
@@ -66,19 +40,23 @@ import Settings from "../component/Settings";
 
                 {/* Main Content Area */}
                 <div className="xl:w-8/10 lg:w-8/10 md:w-10/10 sm:w-full">
+                    {/* Main Navigation Components */}
                     {activeComponent === "dashboard" && <Dashboard />}
                     {activeComponent === "subcribe" && <Subcription />}
                     {activeComponent === "settings" && <Settings />}
                     {activeComponent === "manageExam" && (
                         <ManageExam onCreateNewExam={() => setActiveComponent("newExam")} />
                     )}
+                    {activeComponent === "result" && <ManageResult />}
+                    {activeComponent === "student" && <ManageStudents />}
+
+                    {/* Exam Flow Navigation */}
                     {activeComponent === "newExam" && (
                         <NewExam
                             onBack={() => setActiveComponent("manageExam")}
                             onNext={() => setActiveComponent("addQuestion")}
                         />
                     )}
-
                     {activeComponent === "addQuestion" && (
                         <AddQuestion
                             onBack={() => setActiveComponent("newExam")}
@@ -89,13 +67,12 @@ import Settings from "../component/Settings";
                         <AddStudents
                             onBack={() => setActiveComponent("addQuestion")}
                             onSubmit={handleCreateExam}
-
                         />
                     )}
                 </div>
->>>>>>> 95f3d444caf171b58ad22eb31db068b9133a1b5a
             </div>
-        );
-    };
+        </div>
+    );
+};
 
-    export default Home;
+export default Home;
